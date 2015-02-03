0a083e279d7f53536f8cb3fcba21646a52df2d6d
Brad West <brad@bradonomics.com>
2015-02-03 14:33:57 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Genesis Boilerplate
+Genesis boilerplate is a starter kit for Genesis child themes based on Skeleton CSS.
+
+Check out <http://getskeleton.com> for documentation and details about the Skeleton CSS framework.
+Check out <http://studiopress.com> for documentation and details about the Genesis framework.
+
+## Getting started
+
+To start your new child theme with Genesis Boilerplate, you can [Download the files from GitHub]() or clone the repo: `git clone https://github.com/bradonomics/Genesis-Boilerplate.git`. You can then upload the files under Appearances -> Themes in your Wordpress Dashboard, although you'll probably want to add some styles before you do. Remember this is a starting point for a custom theme and pretty vanilla as it is.
+
+### What's in the download?
+
+The download includes a rewrite of Skeleton CSS to use standard Genesis classes and a functions file to get you started building a custom Genesis child theme.
+
+You'll want to add a [screenshot.png file](http://codex.wordpress.org/Theme_Development#Screenshot) so something will show in the Wordpress Dashboard. You'll probably want to add an /images directory, and if you intend to use Javascript, a /js directory.
+
+## License
+
+[Skeleton](https://github.com/dhg/Skeleton/blob/master/LICENSE.md) and [Genesis Boilerplate]() are released under the open-source MIT license. The Genesis Framework itself is released under the [GPL-2.0+ License](http://www.gnu.org/licenses/gpl-2.0.html).
+
+## Acknowledgements
+
+Skeleton was created by [Dave Gamache](https://twitter.com/dhg).
---
+++ functions.php
@@ -0,0 +1,102 @@
+<?php
+//* Start the engine
+include_once( get_template_directory() . '/lib/init.php' );
+
+//* Child theme info (you can change it if you like)
+define( 'CHILD_THEME_NAME', __( 'Genesis Boilerplate', 'geneplate' ) );
+define( 'CHILD_THEME_URL', 'http://github.com/bradonomics/genesis-boilerplate/' );
+define( 'CHILD_THEME_VERSION', '0.4' );
+
+//* Include Google Fonts
+add_action( 'wp_enqueue_scripts', 'geneplate_enqueue_scripts' );
+function geneplate_enqueue_scripts() {
+  wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Railway', array(), CHILD_THEME_VERSION );
+}
+
+//* Add HTML5 markup structure
+add_theme_support( 'html5' );
+
+//* Add viewport meta tag for mobile browsers
+add_theme_support( 'genesis-responsive-viewport' );
+
+//* Add Structural Wraps
+add_theme_support( 'genesis-structural-wraps', array( 'header', 'footer-widgets' ) );
+
+//* Remove Edit Link
+add_filter( 'edit_post_link', '__return_false' );
+
+//* Remove Comment Reply Script
+add_action( 'wp_enqueue_scripts', 'remove_comment_scripts' );
+function remove_comment_scripts() {
+  wp_dequeue_script( 'comment-reply' );
+}
+
+//* Remove WordPress version
+remove_action( 'wp_head', 'wp_generator' );  
+
+//* Unregister layout settings
+  //* Remove the comment line to activate the removal of any layouts you don't intend to use.
+// genesis_unregister_layout( 'full-width-content' );
+// genesis_unregister_layout( 'content-sidebar' );
+// genesis_unregister_layout( 'sidebar-content' );
+genesis_unregister_layout( 'content-sidebar-sidebar' );
+genesis_unregister_layout( 'sidebar-sidebar-content' );
+genesis_unregister_layout( 'sidebar-content-sidebar' );
+
+//* Remove RSD Link in Header
+remove_action( 'wp_head', 'rsd_link' );
+
+//* Unregister Sidebars
+  //* Remove the comment line to activate the removal of the sidebars if you don't intend to use them.
+// unregister_sidebar( 'sidebar' );
+// unregister_sidebar( 'sidebar-alt' );
+
+//* Remove unused Genesis Widgets
+  //* Add a comment to block or remove the line to deactivate the removal of any widgets you'd like to use.
+add_action( 'widgets_init', 'remove_genesis_widgets', 20 );
+function remove_genesis_widgets() {
+  unregister_widget( 'Genesis_Featured_Page' );
+  unregister_widget( 'Genesis_Featured_Post' );
+  unregister_widget( 'Genesis_User_Profile_Widget' );
+}
+
+//* Hooks after-entry widget area to single posts
+add_action( 'genesis_entry_footer', 'geneplate_after_entry' );
+function geneplate_after_entry() {
+  if ( ! is_singular( 'post' ) )
+    return;
+    genesis_widget_area( 'after-entry', array(
+      'before' => '<div class="after-entry widget-area"><div class="wrap">',
+      'after'  => '</div></div>',
+    ) );
+}
+
+//* Register after-entry widget area
+genesis_register_sidebar( array(
+	'id'			=> 'after-entry',
+	'name'			=> __( 'After Entry', 'geneplate' ),
+	'description'	=> __( 'This is the after post section.', 'geneplate' ),
+) );
+
+//* Add 3-Column Footer Widget Area
+add_theme_support( 'genesis-footer-widgets', 3 );
+
+//* Change the footer text
+remove_action( 'genesis_footer', 'genesis_do_footer' );
+add_action( 'genesis_footer', 'geneplate_footer' );
+function geneplate_footer() {
+	?>
+	<p><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a> &copy; <?php echo date('Y') ?> &middot; <a href="http://github.com/bradonomics/genesis-boilerplate/" rel="nofollow">Built using Genesis Boilerplate</a></p>
+	<?php
+}
+
+//* Add wrap inside entry-content div in case you full-width images are needed.
+add_action( 'genesis_entry_content', 'entry_content_wrap_open', 1 );
+function entry_content_wrap_open() {
+    echo '<div class="wrap">';
+}
+
+add_action( 'genesis_entry_content', 'entry_content_wrap_close', 25 );
+function entry_content_wrap_close() {
+    echo '</div>';
+}
