2be22f8898d978c154901a97bfbd424693e26822
Brad West <brad@bradonomics.com>
2015-04-14 18:53:22 +0700

    Added new inline-block type display for mobile menu.

--- js/responsive-menu.js
+++ js/responsive-menu.js
@@ -1,24 +1,38 @@
-jQuery(function( $ ){
+jQuery( function ( $ ) {
 
-	$("header .genesis-nav-menu, .nav-primary .genesis-nav-menu, .nav-secondary .genesis-nav-menu").addClass("responsive-menu").before('<div class="responsive-menu-icon"></div>');
+	$( '.nav-primary .genesis-nav-menu' ).addClass( 'responsive-menu' ).before( '<div class="responsive-menu-icon"></div>' );
 
-	$(".responsive-menu-icon").click(function(){
-		$(this).next("header .genesis-nav-menu, .nav-primary .genesis-nav-menu, .nav-secondary .genesis-nav-menu").slideToggle();
+	$( '.responsive-menu-icon' ).click( function () {
+		$( this ).next( '.nav-primary .genesis-nav-menu' ).slideToggle();
 	});
 
-	$(window).resize(function(){
-		if(window.innerWidth > 550) {
-			$("header .genesis-nav-menu, .nav-primary .genesis-nav-menu, .nav-secondary .genesis-nav-menu, nav .sub-menu").removeAttr("style");
-			$(".responsive-menu > .menu-item").removeClass("menu-open");
-		}
-	});
-
-	$(".responsive-menu > .menu-item").click(function(event){
-		if (event.target !== this)
-		return;
-			$(this).find(".sub-menu:first").slideToggle(function() {
-			$(this).parent().toggleClass("menu-open");
+	$( '.responsive-menu' ).on( 'click', '.menu-item', function ( event ) {
+		if ( event.target !== this )
+			return;
+		$( this ).find( '.sub-menu:first' ).slideToggle( function () {
+			$( this ).parent().toggleClass( 'menu-open' );
 		});
 	});
+
+	setupMenus();
+
+	$( window ).resize( function () {
+		setupMenus();
+	});
+
+	function setupMenus() {
+		if ( window.innerWidth <= 549 ) {
+			$( 'ul.menu-secondary > li' ).addClass( 'moved-item' ); // tag moved items so we can move them back
+			$( 'ul.menu-secondary > li' ).appendTo( 'ul.menu-primary' );
+			$( '.nav-secondary' ).hide();
+		}
+
+		if ( window.innerWidth > 549 ) {
+			$( '.nav-primary .genesis-nav-menu, nav .sub-menu' ).removeAttr( 'style' );
+			$( '.responsive-menu > .menu-item' ).removeClass( 'menu-open' );
+			$( '.nav-secondary' ).show();
+			$( 'ul.menu-primary > li.moved-item' ).appendTo( 'ul.menu-secondary' );
+		}
+	}
 
 });
