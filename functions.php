188593d243193dda6cc1f7d597caafee08a92934
bradonomics <brad@bradonomics.com>
2015-10-13 16:49:38 +0700

    Minor edits

--- functions.php
+++ functions.php
@@ -171,6 +171,6 @@ function sidebar_wrap_close() {
 //* Custom footer text and copyright info.
 remove_action( 'genesis_footer', 'genesis_do_footer' );
 add_action( 'genesis_footer', 'geneplate_footer' );
-function geneplate_footer() {
-  ?><p><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a> &copy; <?php echo date('Y') ?> &middot; <a href="http://github.com/bradonomics/genesis-boilerplate/" rel="nofollow">Built using Genesis Boilerplate</a></p><?php
+function geneplate_footer() { ?>
+  <p><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a> &copy; <?php echo date('Y') ?> &middot; <a href="http://github.com/bradonomics/genesis-boilerplate/" rel="nofollow">Built using Genesis Boilerplate</a></p><?php
 }
