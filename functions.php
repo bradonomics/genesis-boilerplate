6838f865cf3a6323e309ba343d3c642f424e04e4
bradonomics <brad@bradonomics.com>
2015-11-26 16:34:06 +0700

    Removed recent comment inline styles from head

--- functions.php
+++ functions.php
@@ -69,6 +69,13 @@ remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 remove_action( 'wp_print_styles', 'print_emoji_styles' );
 remove_action( 'admin_print_styles', 'print_emoji_styles' );
 
+//* Remove recent comment sytles from head when using widgets
+add_action( 'widgets_init', 'remove_recent_comments_style' );
+function remove_recent_comments_style() {
+    global $wp_widget_factory;
+    remove_action( 'wp_head', array( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style' ) );
+}
+
 
 /************* LAYOUTS AND WIDGETS *************/
 
